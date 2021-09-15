a04503a69ebbb4cbbbb0a943bceb97ab9e67f061
ferrgz01 <ferrodriguezs016@gmail.com>
2021-09-15 14:12:47 -0500

    Proyecto agregado

---
+++ src/operacionesact5/OperacionesAct5.java
@@ -0,0 +1,37 @@
+
+package operacionesact5;
+
+/**
+ *
+ * @author ferro
+ */
+public class OperacionesAct5 {
+
+    
+    public static void main(String[] args) {
+        int x=20;
+        int y=5;
+        
+        //Suma
+        int suma = x+y;
+        //Resta
+        int res=x-y;
+        //Multiplicación
+        int mul=x*y;
+        //División
+        int div=x/y; 
+        //Módulo
+        int mod=x%y;
+        
+        System.out.println("X = "+x);
+        System.out.println("y = "+y);
+        System.out.println("Suma = "+suma);
+        System.out.println("Resta = "+res);
+        System.out.println("Multiplicación = "+mul);
+        System.out.println("División (X/Y) = "+div);
+        System.out.println("Módlo (X/Y) = "+mod);
+        
+        
+    }
+    
+}
